98dd905785350813c1599b7d7ec9c7508d6dea25
MatthieuJacobs <43750283+MatthieuJacobs@users.noreply.github.com>
2020-03-06 09:57:59 +0100

    Update loop_closure_eqs_8bar.m

--- loop_closure_eqs_8bar.m
+++ loop_closure_eqs_8bar.m
@@ -7,7 +7,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function F=loop_closure_eqs(phi_init,phi1,r0,r1,r2,r5,r6,AE,FC,BF,EB)
+function F=loop_closure_eqs_8bar(phi_init,phi1,r0,r1,r2,r5,r6,AE,FC,BF,EB,phi0)
 
 % first argument: the initial values of the unknown angles and unknown
 % distance x7
@@ -24,10 +24,9 @@ phi6 = phi_init(5);
 x7 = phi_init(6);
 
 % loop closure equations:
-F(1)=AE*cos(phi3)-r6*cos(phi6)-r2*cos(phi2)-r0+r1*cos(phi1);
-F(2)=AE*sin(phi3)+r1*sin(phi1)-r6*sin(phi6)-r2*sin(phi2);
-F(3)=r2*cos(phi2)+r5*cos(phi5)+FC*cos(phi4)-x7;
-F(4)=r2*sin(phi2)+r5*sin(phi5)+FC*sin(phi4);
+F(1)=AE*cos(phi3)-r6*cos(phi6)-r2*cos(phi2)-r0*cos(phi0)+r1*cos(phi1);
+F(2)=AE*sin(phi3)+r1*sin(phi1)-r6*sin(phi6)-r2*sin(phi2)-r0*sin(phi0);
+F(3)=r2*cos(phi2)+r5*cos(phi5)+FC*cos(phi4)-x7*cos(phi0);
+F(4)=r2*sin(phi2)+r5*sin(phi5)+FC*sin(phi4)-x7*sin(phi0);
 F(5)=r6*cos(phi6)+EB*cos(phi3)+BF*cos(phi4)-r5*cos(phi5);
 F(6)=r6*sin(phi6)+EB*sin(phi3)+BF*sin(phi4)-r5*sin(phi5);
-
